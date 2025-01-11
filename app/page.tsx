db66be6b0c13e86ab29cdfdffc521f5afd5a1919
Javan Miller <javan@fastmail.com>
2025-01-11 09:49:05 -0500

    Move Footer component

--- app/page.tsx
+++ app/page.tsx
@@ -184,16 +184,6 @@ export default function Home() {
           </div>
         )}
       </main>
-      <footer className="pb-4 w-full mt-12">
-        <div className="w-full px-4 flex justify-between items-center">
-          <p className="text-black dark:text-white">
-            © 2025 <a href="https://www.javanmiller.com">Javan Miller</a>
-          </p>
-          <div className="text-black dark:text-white">
-            <a href="https://www.javanmiller.com/blog/ScriptureScope">Learn More</a>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 }

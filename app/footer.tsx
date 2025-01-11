fff65057e6d964e175608c504aa0b424e01b09d5
Javan Miller <javan@fastmail.com>
2025-01-11 16:14:42 -0500

    Move Footer to separate tsx file
    * Move Footer component
    
    * Add separate file for Footer function

---
+++ app/footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+      <div className="flex flex-col max-w-4xl mx-auto">
+        <footer className="pb-4 w-full mt-12">
+          <div className="w-full px-4 flex justify-between items-center">
+            <p className="text-black dark:text-white">
+              © 2025 <a href="https://www.javanmiller.com">Javan Miller</a>
+            </p>
+            <div className="text-black dark:text-white">
+              <a href="https://www.javanmiller.com/blog/ScriptureScope">Learn More</a>
+            </div>
+          </div>
+        </footer>
+      </div>
+    );
+  };
+  
+  export default Footer;

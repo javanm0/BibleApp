824b08f19116111d3e877d5fc2eb518d0cff51e7
Javan Miller <javan@fastmail.com>
2025-01-22 20:17:10 -0500

    Remove Footer from Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
-import Footer from "./footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        {/* <Footer /> */}
         <Analytics />
       </body>
     </html>
